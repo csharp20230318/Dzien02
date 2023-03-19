c11bb4445605d122bdc1bcc2811574b1863e13ee
kurs <kurs@DESKTOP-V3HDKPQ>
2023-03-19 12:40:22 +0100

    Add project files.

---
+++ Serializacja/BinarySerializer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serializacja
+{
+    internal class BinarySerializer
+    {
+        public static void Create()
+        {
+            Employee emp = new Employee()
+            {
+                Id = 123,
+                FirstName = "Jan",
+                LastName = "Kowalski",
+                AccessRooms = new List<int>() { 2, 3, 4 },
+                IsManager = false,
+                StartAt = new DateTime(2022, 1,1)
+            };
+            emp.SetToken(Guid.NewGuid().ToString());
+
+            // serializacja
+            using (FileStream fs = new FileStream("dump.bin", FileMode.Create))
+            {
+                BinaryFormatter bf = new BinaryFormatter();
+                bf.Serialize(fs, emp);
+            }
+
+            // deserializacja
+            using (FileStream fs = new FileStream("dump.bin", FileMode.Open))
+            {
+                BinaryFormatter bf = new BinaryFormatter();
+                Employee empDeserial = bf.Deserialize(fs) as Employee;
+                if (empDeserial != null)
+                {
+                    Console.WriteLine(empDeserial);
+                }
+            }
+
+
+        }
+
+    }
+}
---
+++ Serializacja/Employee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serializacja
+{
+    [Serializable]
+    internal class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public bool? IsManager { get; set; }
+        public List<int> AccessRooms { get; set; }
+        public DateTime? StartAt { get; set; }
+
+        public List<string> ExtraData { get; set; }
+
+        private string Token;
+
+        public void SetToken(string token)
+        {
+            Token = token;
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName}";
+        }
+
+    }
+}
